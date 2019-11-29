da12c13967028fed9ead06cc6fcd2f3a0bdb03ce
Lhakpa Sherpa <sherpal@spu.edu>
2019-11-28 21:49:15 -0800

    scroll only when skills section in view

--- js/script.js
+++ js/script.js
@@ -33,15 +33,22 @@ $(document).ready(function() {
       }
     });
 
-    $('.chart').easyPieChart({
-      easing: 'easeInOut',
-      barColor: '#fff',
-      trackColor: false,
-      scaleColor: false,
-      lineWidth: 4,
-      size: 152,
-      onStep: function(from, to, percent){
-        $(this.el).find('.percent').text(Math.round(percent));
+    var skillsTopOffset= $("skillsSection").offset().top;
+    $(window).scroll(fucntion(){
+      if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+        $('.chart').easyPieChart({
+          easing: 'easeInOut',
+          barColor: '#fff',
+          trackColor: false,
+          scaleColor: false,
+          lineWidth: 4,
+          size: 152,
+          onStep: function(from, to, percent){
+            $(this.el).find('.percent').text(Math.round(percent));
+          }
+        });s
       }
+
     });
+
 });
