e0330a62bbe03b247a07a56a0b43fe5338fc4671
Lhakpa Sherpa <sherpal@spu.edu>
2019-11-28 20:59:53 -0800

    Basic ow carousel item setup

--- js/script.js
+++ js/script.js
@@ -14,5 +14,22 @@ $(document).ready(function() {
       showCursor: false
     });
 
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+    });
+
 
 });
