7067f93038216d9d82dfa971f452c786f748ac92
sherpaj <sherpal@spu.edu>
2020-08-17 20:07:52 -0700

    formspree.io form setup

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@
     skillFill();
     portfolioItemContentLoadOnClick();
     setParallax();
-    sendMail();
     fitVideo();
 
     $(window).on('load', function () {
@@ -33,7 +32,26 @@
         setActiveMenuItem();
     });
 
-
+    window.addEventListener("DOMContentLoaded", function() {
+      
+        // get the form elements defined in your form HTML above
+        
+        var form = document.getElementById("contact-form");
+        var button = document.getElementById("contact-submit-holder");
+        var status = document.getElementById("my-form-status");
+    
+        // Success and Error functions for after the form is submitted
+        
+        function success() {
+          form.reset();
+          button.style = "display: none ";
+          status.innerHTML = "Thanks!";
+        }
+    
+        function error() {
+          status.innerHTML = "Oops! There was a problem.";
+        }
+      });
 //------------------------------------------------------------------------
 //Helper Methods -->
 //------------------------------------------------------------------------
@@ -282,65 +300,4 @@
         var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
         return pattern.test(emailAddress);
     }
-
-    function sendMail() {
-        $('.contact-form [type="submit"]').on('click', function () {
-
-            var emailVal = $('#contact-email').val();
-
-            if (isValidEmailAddress(emailVal)) {
-                var params = {
-                    'action': 'SendMessage',
-                    'name': $('#name').val(),
-                    'email': $('#contact-email').val(),
-                    'subject': $('#subject').val(),
-                    'message': $('#message').val()
-                };
-                $.ajax({
-                    type: "POST",
-                    url: "php/sendMail.php",
-                    data: params,
-                    success: function (response) {
-                        if (response) {
-                            var responseObj = $.parseJSON(response);
-                            if (responseObj.ResponseData)
-                            {
-                                alert(responseObj.ResponseData);
-                            }
-                        }
-                    },
-                    error: function (xhr, ajaxOptions, thrownError) {
-                        //xhr.status : 404, 303, 501...
-                        var error = null;
-                        switch (xhr.status)
-                        {
-                            case "301":
-                                error = "Redirection Error!";
-                                break;
-                            case "307":
-                                error = "Error, temporary server redirection!";
-                                break;
-                            case "400":
-                                error = "Bad request!";
-                                break;
-                            case "404":
-                                error = "Page not found!";
-                                break;
-                            case "500":
-                                error = "Server is currently unavailable!";
-                                break;
-                            default:
-                                error = "Unespected error, please try again later.";
-                        }
-                        if (error) {
-                            alert(error);
-                        }
-                    }
-                });
-            } else {
-                alert('Your email is not in valid format');
-            }
-        });
-    }
-
 }(jQuery));
